4630840dfead5e13c0db4299eb4ce2235e2a593c
DaviVeigaJardim <davijardimrp@gmail.com>
2025-04-13 15:02:18 -0300

    Adiciona opcoes de pre processamento

--- OcrApp.h
+++ OcrApp.h
@@ -1,31 +1,38 @@
-
-
-#pragma once
+#ifndef OCRAPP_H
+#define OCRAPP_H
 
 #include <QWidget>
 #include <QLabel>
-#include <QPushButton>
 #include <QTextEdit>
-#include <QFileDialog>
-#include <opencv2/opencv.hpp>
+#include <QPushButton>
+#include <QCheckBox>
 #include <tesseract/baseapi.h>
+#include <opencv2/core.hpp>
 
 class OCRApp : public QWidget {
     Q_OBJECT
 
 public:
     OCRApp(QWidget *parent = nullptr);
+    ~OCRApp();
 
 private slots:
     void loadImage();
     void runOCR();
+    void updateProcessedImage();
 
 private:
     QLabel *imageLabel;
+    QLabel *processedImageLabel;
     QTextEdit *textEdit;
     QPushButton *loadButton;
     QPushButton *ocrButton;
-    cv::Mat image;
+    QCheckBox *binarizationCheckBox;
+    QCheckBox *grayScaleCheckBox;
     tesseract::TessBaseAPI *tess;
+    cv::Mat image;
+
+    cv::Mat preprocessImage(const cv::Mat& inputImage);
 };
 
+#endif // OCRAPP_H
